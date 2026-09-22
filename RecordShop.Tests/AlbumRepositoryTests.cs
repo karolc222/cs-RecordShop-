35ff31270018239d9eaaa4d2643ab4b88183c9fd
karolc222 <carolina.lupanciuc@gmail.com>
2026-09-22 17:41:37 +0100

    Testing the Repository, GetAllAlbums method.

--- RecordShop.Tests/AlbumRepositoryTests.cs
+++ RecordShop.Tests/AlbumRepositoryTests.cs
@@ -11,25 +11,28 @@ public class AlbumRepositoryTests
     [Fact]
     public async Task GetAllAlbumsAsync_ReturnsAllAlbums()
     {
+        //fake db
         var options = new DbContextOptionsBuilder<AppDbContext>()
-    .UseInMemoryDatabase(databaseName: "TestDatabase")
-    .Options;
-    
+            .UseInMemoryDatabase(databaseName: "TestRepositoryDatabase")
+            .Options;
+
+        //add test data
         using (var context = new AppDbContext(options))
         {
-            context.Albums.Add(new Album { AlbumId = 1, AlbumTitle = "Test Album 1", ArtistId = 1, ReleaseDate = DateTime.Now, Stock = 10 });
-            context.Albums.Add(new Album { AlbumId = 2, AlbumTitle = "Test Album 2", ArtistId = 2, ReleaseDate = DateTime.Now, Stock = 5 });
+            context.Albums.Add(new Album { AlbumId = 1, AlbumTitle = "A discovery of witches", ArtistId = 1, ReleaseDate = DateTime.Now, Stock = 10 });
+            context.Albums.Add(new Album { AlbumId = 2, AlbumTitle = "Dune", ArtistId = 2, ReleaseDate = DateTime.Now, Stock = 5 });
             await context.SaveChangesAsync();
         }
-
+        
+        //creating repository and testing the GetAllAlbumsAsync method
         using (var context = new AppDbContext(options))
         {
             var repository = new AlbumRepository(context);
             var albums = await repository.GetAllAlbumsAsync();
 
             Assert.Equal(2, albums.Count);
-            Assert.Equal("Test Album 1", albums[0].AlbumTitle);
-            Assert.Equal("Test Album 2", albums[1].AlbumTitle);
+            Assert.Equal("A discovery of witches", albums[0].AlbumTitle);
+            Assert.Equal("Dune", albums[1].AlbumTitle);
         }
 
     }
